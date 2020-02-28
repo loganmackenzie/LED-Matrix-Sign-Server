2c880d3db82ae5aa1e8ea316d8a6501e89ea6d4b
Logan MacKenzie <logan.mackenzie@geckorobotics.com>
2020-02-27 22:52:06 -0500

    Fix route responses

--- sign_server.py
+++ sign_server.py
@@ -14,21 +14,25 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     led_sign.message = 'Home!'
+    return 'OK\n', 200
 
 
 @app.route('/display/<string:message>')
 def display(message):
     led_sign.message = message
+    return message, 200
 
 
 @app.route('/clear')
 def clear():
     led_sign.clear()
+    return 'OK\n', 200
 
 
 @app.route('/color/<string:color>')
 def set_color(color):
     led_sign.color = color
+    return 'OK\n', 200
 
 
 if __name__ == '__main__':
